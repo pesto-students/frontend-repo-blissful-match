210b330778af7ac63d7c2b1da0155d663b85c16c
edrischhawniwala <echhawniwala@gmail.com>
2024-09-07 14:07:57 +0530

    My View History Updates

--- src/pages/MyViewHistory/MyViewHistory.tsx
+++ src/pages/MyViewHistory/MyViewHistory.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Profile, profiles as demoProfiles } from './Profile';
+import { UserProfile, ViewedProfilesResponse } from './Profile';
 import MembersCard from './InnerComponents/MembersCard';
 import Footer from '@components/Footer/Footer';
 import Navbar from '@components/Navbar/Navbar';
@@ -9,12 +9,14 @@ import MyProfileTabs from '@components/MyProfileTabs/MyProfileTabs';
 
 const MyViewHistory: React.FC = () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [profiles, setProfiles] = React.useState(demoProfiles);
+    const [profiles, setProfiles] = React.useState<Array<UserProfile>>();
+    const [viewedProfilesResponse, setViewedProfilesResponse] =
+        React.useState<ViewedProfilesResponse>();
     const [isLoaded, setIsLoaded] = React.useState(false);
     const getProfiles = async () => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const response = await getViewHistory({ pageSize: 100, pageNumber: 1 });
-        // console.log(response);
+        setViewedProfilesResponse(response);
+        setProfiles(response.data);
     };
 
     React.useEffect(() => {
@@ -26,20 +28,32 @@ const MyViewHistory: React.FC = () => {
 
     return (
         <div className="bg-white">
-            <Navbar />
-            <Banner title="My View History" />
-            <MyProfileTabs activeTab="MyViewHistory"></MyProfileTabs>
-            <div className="mt-4 min-h-screen ">
-                <div className="flex justify-center">
-                    <main className="bg-white w-3/4 p-4 grid grid-cols-1 gap-6 main-history">
-                        {profiles.map((profile: Profile) => (
-                            <MembersCard key={profile.id} profile={profile} />
-                        ))}
-                    </main>
-                </div>
+            {viewedProfilesResponse && profiles ? (
+                <>
+                    <Navbar />
+                    <Banner title="My View History" />
+                    <MyProfileTabs
+                        activeTab="MyViewHistory"
+                        balance={`${viewedProfilesResponse?.usedViewedLimit}/${viewedProfilesResponse?.totalViewedLimit}`}
+                    ></MyProfileTabs>
+                    <div className="mt-4 min-h-screen ">
+                        <div className="flex justify-center">
+                            <main className="bg-white w-3/4 p-4 grid grid-cols-1 gap-6 main-history">
+                                {profiles.map((profile: UserProfile) => (
+                                    <MembersCard
+                                        key={profile.user_id}
+                                        profile={profile}
+                                    />
+                                ))}
+                            </main>
+                        </div>
 
-                <Footer />
-            </div>
+                        <Footer />
+                    </div>
+                </>
+            ) : (
+                ''
+            )}
         </div>
     );
 };
